758d18adc3adbcab03f554218357e7fe1fecb769
Valeriano Chete <vjchete@gmail.com>
2024-09-11 22:53:34 -0600

    Access - add access auth

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -18,7 +18,7 @@ class DatabaseSeeder extends Seeder
         $this->call(AuthModulesSeeder::class);
         $this->call(AuthModulesPermissionsSeeder::class);
         $this->call(AuthMenuSeeder::class);
-        $this->call(AdminSeeder::class);
+        $this->call(AuthAdminSeeder::class);
         Schema::enableForeignKeyConstraints();
 
     }
