bae22ec6dd1d1ff3805000f54820963abd4973e3
Valeriano Chete <vjchete@gmail.com>
2024-11-19 18:06:05 -0600

    Esential Crud - lang publish

---
+++ src/Providers/EssentialCrudServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+namespace Trueandfalse\Essential\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class EssentialCrudServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot(\Illuminate\Contracts\Http\Kernel $kernel): void
+    {
+        $this->publishes([
+            __DIR__ . '/../resources/lang' => $this->app->langPath('/'),
+        ], 'essencrud-lang');
+    }
+}
---
+++ src/resources/lang/es/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | outcome such as failure due to an invalid password / reset token.
+    |
+     */
+
+    'reset'     => 'Su contraseña ha sido cambiada.',
+    'sent'      => 'Le hemos enviado un correo de cambio de contraseña!',
+    'throttled' => 'Por favor, espera antes de intentarlo de nuevo',
+    'token'     => 'El código de cambio de contraseña es inválido.',
+    'user'      => 'No podemos encontrar un usuario con ese nombre.',
+
+];
